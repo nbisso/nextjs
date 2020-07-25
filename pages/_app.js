58408607fce430260296d09874d4853065dff5a7
Luciano Lupo <lucianolupo97@gmail.com>
2020-07-25 16:20:57 -0300

    WIP

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,65 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
 
-export default function MyApp(props) {
-    const { Component, pageProps } = props;
+// export default function MyApp(props) {
+//     const { Component, pageProps } = props;
 
-    React.useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-            jssStyles.parentElement.removeChild(jssStyles);
-        }
-    }, []);
+//     React.useEffect(() => {
+//         // Remove the server-side injected CSS.
+//         const jssStyles = document.querySelector('#jss-server-side');
+//         if (jssStyles) {
+//             jssStyles.parentElement.removeChild(jssStyles);
+//         }
+//     }, []);
 
-    return (
-        <React.Fragment>
-            <Head>
-                <title>My page</title>
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-            </Head>
-            <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </React.Fragment>
+//     return (
+
+//     );
+// }
+
+// MyApp.propTypes = {
+//     Component: PropTypes.elementType.isRequired,
+//     pageProps: PropTypes.object.isRequired,
+// };
+
+import React from "react";
+import App from "next/app";
+//import SiteLayout from "../components/SiteLayout";
+//import "../css/tailwind.css";
+const CleanLayout = ({ children }) => (
+  <div className="bg-white antialiased">
+    <div className="mt-6 sm:mt-0 sm:py-12">{children}</div>
+  </div>
+);
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, router } = this.props;
+
+    const getLayout =
+      Component.getLayout || ((page) => <CleanLayout children={page} />);
+
+    return getLayout(
+      <React.Fragment>
+        <Head>
+          <title>My page</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </React.Fragment>
     );
+  }
 }
 
-MyApp.propTypes = {
-    Component: PropTypes.elementType.isRequired,
-    pageProps: PropTypes.object.isRequired,
-};
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,67 @@
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
-export default function Home({ stars }) {
-    const router = useRouter()
+// export default function Home({ stars }) {
+//     const router = useRouter()
 
-    const [loading, setLoading] = useState(true)
+//     const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        async function loadUserFromCookies() {
-            await new Promise(r => {
-                setTimeout(() => {
-                    r()
-                }, 2000);
-            })
-            setLoading(false)
-        }
-        loadUserFromCookies()
-    }, [])
-    function login() {
-        fetch("/api/login").then(async r => {
-            console.log(r)
-        })
-    }
+//     useEffect(() => {
+//         async function loadUserFromCookies() {
+//             await new Promise(r => {
+//                 setTimeout(() => {
+//                     r()
+//                 }, 2000);
+//             })
+//             setLoading(false)
+//         }
+//         loadUserFromCookies()
+//     }, [])
+//     function login() {
+//         fetch("/api/login").then(async r => {
+//             console.log(r)
+//         })
+//     }
 
-    return (
-        <div>
-            <h1>Hello nextjs 2.0</h1>
-            {stars}
-            <div>{loading ? "cargando..." : ""}</div>
-            <button onClick={login}>login</button>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <h1>Hello nextjs 2.0</h1>
+//             {stars}
+//             <div>{loading ? "cargando..." : ""}</div>
+//             <button onClick={login}>login</button>
+//         </div>
+//     )
+// }
 
-Home.getInitialProps = async (ctx) => {
-    const res = await fetch('https://api.github.com/repos/vercel/next.js')
-    const json = await res.json()
-    return { stars: json.stargazers_count }
-}
+// Home.getInitialProps = async (ctx) => {
+//     const res = await fetch('https://api.github.com/repos/vercel/next.js')
+//     const json = await res.json()
+//     return { stars: json.stargazers_count }
+// }
+
+import Link from "next/link";
+import { getLayout } from "../components/SiteLayout";
+
+const Index = () => (
+  <div className="mt-8 max-w-xl mx-auto px-8">
+    <h1 className="text-center">
+      <span className="block text-xl text-gray-600 leading-tight">
+        Welcome to this
+      </span>
+      <span className="block text-5xl font-bold leading-none">
+        Awesome Website
+      </span>
+    </h1>
+    <div className="mt-12 text-center">
+      <Link href="/account-settings/basic">
+        <a className="inline-block bg-gray-900 hover:bg-gray-800 text-white font-medium rounded-lg px-6 py-4 leading-tight">
+          View account settings
+        </a>
+      </Link>
+    </div>
+  </div>
+);
+
+Index.getLayout = getLayout;
+
+export default Index;
