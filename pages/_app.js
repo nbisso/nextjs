65c0afe04f5c679bd716dc85788691d54e627b98
nbisso <nicolas.bisso@mercadolibre.com>
2020-07-25 23:01:24 -0300

    firebase front config

--- pages/_app.js
+++ pages/_app.js
@@ -9,14 +9,14 @@ import SiteLayout from "../components/layouts/SiteLayout";
 import theme from "../src/theme";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCwk-9Lo6_9k2LZGk6FrH-n4rWXv3xWvrQ",
-  authDomain: "cocinando-766f0.firebaseapp.com",
-  databaseURL: "https://cocinando-766f0.firebaseio.com",
-  projectId: "cocinando-766f0",
-  storageBucket: "cocinando-766f0.appspot.com",
-  messagingSenderId: "311671787189",
-  appId: "1:311671787189:web:1bb431b96c47d3c69af8fc",
-  measurementId: "G-TM68NRZNL0"
+  apiKey: "AIzaSyCq1D2qapuqgiHhuRzgkymRt7eZy0W_-Fg",
+  authDomain: "cocinandoapp-a5abb.firebaseapp.com",
+  databaseURL: "https://cocinandoapp-a5abb.firebaseio.com",
+  projectId: "cocinandoapp-a5abb",
+  storageBucket: "cocinandoapp-a5abb.appspot.com",
+  messagingSenderId: "101142323539",
+  appId: "1:101142323539:web:02719f8d0f93d9d85dc1a6",
+  measurementId: "G-BR0DG9HZK9"
 };
 
 if (!firebase.apps.length) {
