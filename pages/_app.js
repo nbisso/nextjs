b232d3fd7525418865f8e79b2209dc6a102ee78b
nbisso <nicolas.bisso@mercadolibre.com>
2020-07-26 19:44:30 -0300

    add enviroment config on app

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ import theme from "../src/theme";
 function MyApp(props) {
 
 
-  var firebaseConfig = props.enviroment.FIREBASE_FRONT_CONFIG;
+  var firebaseConfig = props.enviroment;
 
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
@@ -45,6 +45,15 @@ function MyApp(props) {
 }
 
 MyApp.getInitialProps = async (x) => {
-  return { enviroment: require("../util/enviroment") };
+  return { enviroment: process.env.FIREBASE_FRONT_CONFIG ? JSON.parse(process.env.FIREBASE_FRONT_CONFIG) : {
+    apiKey: "AIzaSyCq1D2qapuqgiHhuRzgkymRt7eZy0W_-Fg",
+    authDomain: "cocinandoapp-a5abb.firebaseapp.com",
+    databaseURL: "https://cocinandoapp-a5abb.firebaseio.com",
+    projectId: "cocinandoapp-a5abb",
+    storageBucket: "cocinandoapp-a5abb.appspot.com",
+    messagingSenderId: "101142323539",
+    appId: "1:101142323539:web:02719f8d0f93d9d85dc1a6",
+    measurementId: "G-BR0DG9HZK9"
+} };
 }
 export default MyApp;
